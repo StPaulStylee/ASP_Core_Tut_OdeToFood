d779db5f09aa6b29358a67d3395107acc222f8bd
Jeffrey Miller <miller.s.jeffrey@gmail.com>
2019-05-26 06:09:08 -0500

    Constructing projects that will make up the app

---
+++ OdeToFood/OdeToFood/OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class ListModel : PageModel
+    {
+        private readonly IConfiguration configuartion;
+
+        public ListModel(IConfiguration configuartion)
+        {
+            this.configuartion = configuartion;
+        }
+
+        public string Message { get; set; }
+        public void OnGet()
+        {
+            Message = configuartion["Message"];
+        }
+    }
+}
