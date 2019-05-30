27975df03621af1258882a6f97979375ddfa8141
Jeffrey Miller <miller.s.jeffrey@gmail.com>
2019-05-30 12:04:18 -0500

    adding data interface; other changes

---
+++ OdeToFood/OdeToFood/OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,36 @@
+﻿using OdeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetRestaurantsByName(string name);
+    }
+
+    // This will be for development only; setting us up to easily tranfser this to DB access
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> Restaurants;
+        public InMemoryRestaurantData()
+        {
+            Restaurants = new List<Restaurant>()
+            {
+                new Restaurant { Id = 1, Name = "Jeff's Pizza", Location = "Saint Paul, MN", Cuisine = CuisineType.Italian },
+                new Restaurant { Id = 2, Name = "Emily's Curry", Location = "Arlington, VA", Cuisine = CuisineType.Indian},
+                new Restaurant { Id = 3, Name = "Graham's Crackers", Location = "Minneapolis, MN", Cuisine = CuisineType.Mexican}
+            };
+        }
+        // The parameter is option becaue of the default value of null
+        public IEnumerable<Restaurant> GetRestaurantsByName(string name = null)
+        {
+            return Restaurants.Select(r => r)
+                .Where(string.IsNullOrEmpty(name) || (r) => r.Name.StartsWith(name))
+                .OrderBy(r => r.Id)
+                .ToList();
+        } 
+    }
+}
--- OdeToFood/OdeToFood/OdeToFood/Pages/Restaurants/List.cshtml.cs
+++ OdeToFood/OdeToFood/OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -5,22 +5,38 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
+using OdeToFood.Core;
+using OdeToFood.Data;
 
 namespace OdeToFood.Pages.Restaurants
 {
     public class ListModel : PageModel
     {
         private readonly IConfiguration configuartion;
+        private readonly IRestaurantData restaurantData;
 
-        public ListModel(IConfiguration configuartion)
+        // We are able to pass IRestaurantData here because we added it to the services in Startup.cs
+        public ListModel(IConfiguration configuartion, IRestaurantData restaurantData)
         {
             this.configuartion = configuartion;
+            this.restaurantData = restaurantData;
         }
 
         public string Message { get; set; }
-        public void OnGet()
+        public IEnumerable<Restaurant> Restaurants { get; set; }
+
+        // Here we are passing the name of the input propery we want to target from our view, this
+        // is done for model binding purposes. The param name must match the name of the input
+        public void OnGet(string searchTerm)
         {
+            // This gives us data from the HttpRequest that has been invoed
+            //HttpContext.Request...
+
+            // BUt we want to use this for this particular instance...Model Binding - The goal is to move 
+            // data from a request into an Input Model Binder
+
             Message = configuartion["Message"];
+            Restaurants = restaurantData.GetRestaurantsByName(searchTerm);
         }
     }
 }
