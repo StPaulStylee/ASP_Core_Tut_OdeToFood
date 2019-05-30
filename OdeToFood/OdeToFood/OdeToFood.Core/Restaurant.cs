e0ee18351baea503a7060653f7c67e338dcd36f2
Jeffrey Miller <miller.s.jeffrey@gmail.com>
2019-05-30 17:02:48 -0500

    Add validation requirements to our Restaurant model; implement error dialogs of those validation requirements are not met

--- OdeToFood/OdeToFood/OdeToFood.Core/Restaurant.cs
+++ OdeToFood/OdeToFood/OdeToFood.Core/Restaurant.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace OdeToFood.Core
 {
     public class Restaurant
     {
+        // This attribute will require that this property always be set
+        // This is useful for HTTP requests where data is being entered/edited
+        [Required, StringLength(80)]
         public string Name { get; set; }
+        [StringLength(255)]
         public string Location { get; set; }
         public int Id { get; set; }
         public CuisineType Cuisine { get; set; }
--- OdeToFood/OdeToFood/OdeToFood/Pages/Restaurants/Edit.cshtml.cs
+++ OdeToFood/OdeToFood/OdeToFood/Pages/Restaurants/Edit.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using OdeToFood.Core;
 using OdeToFood.Data;
 
@@ -12,14 +13,26 @@ namespace OdeToFood.Pages.Restaurants
     public class EditModel : PageModel
     {
         private readonly IRestaurantData restaurantData;
-        public Restaurant Restaurant { get; set; }
+        private readonly IHtmlHelper htmlHelper;
 
-        public EditModel(IRestaurantData restaurantData)
+        // This attribute will only execute on a post operation
+        [BindProperty]
+        public Restaurant Restaurant { get; set; }
+        // The type SelectListItem is required for the views' asp-items tag helper
+        public IEnumerable<SelectListItem> Cuisines { get; set; }
+
+        // Remember, we can give the page model/controller additional services by adding them to the
+        // constructor... as we have with IHtmlHelper
+        public EditModel(IRestaurantData restaurantData, IHtmlHelper htmlHelper)
         {
             this.restaurantData = restaurantData;
+            this.htmlHelper = htmlHelper;
         }
         public IActionResult OnGet(int restaurantId)
         {
+            // Don't forget that these Cuisines need to be populated on every type of HTTP request
+            // This is because ASP.NetCore is stateless - you are responsible for setting up your state!
+            Cuisines = htmlHelper.GetEnumSelectList<CuisineType>();
             Restaurant = restaurantData.GetById(restaurantId);
             // If restaurant is not found, redirect to the not found page
             if (Restaurant == null)
@@ -28,6 +41,20 @@ namespace OdeToFood.Pages.Restaurants
             }
             // Otherwise render the related view (Detail.cshtml)
             return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            if(ModelState.IsValid)
+            {
+                // Don't forget that these Cuisines need to be populated on every type of HTTP request
+                // This is because ASP.NetCore is stateless - you are responsible for setting up your state!
+                Cuisines = htmlHelper.GetEnumSelectList<CuisineType>();
+                restaurantData.Update(Restaurant);
+            }
+            restaurantData.Commit();
+            return Page();
+            //return RedirectToPage("./List");
         }
     }
 }
